c1f120e87f61c244d9d942e7fbb0ce6c1cd97d8d
John Nelson <jrnelson654@gmail.com>
2022-02-08 00:13:18 -0600

    Updates Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/" style={{
+                <Link to="/" style={{
                     color: "white",
                     backgroundColor: '#3567f1',
                     borderRadius: '8px'
-                }}>Home</a>
-                <a href="/create" style={{
+                }}>Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: '#3567f1',
                     borderRadius: '8px'
-                }}>Create New</a>
+                }}>Create New</Link>
             </div>
         </nav>
     );
