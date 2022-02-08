161acaed5588e407a1e9d28eb4b5c4e3023929a9
John Nelson <jrnelson654@gmail.com>
2022-02-08 15:04:39 -0600

    Adds initial create form with styling

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New Post</h2>
+            <form>
+                <label>Post title:</label>
+                <input
+                  type="text"
+                  required
+                />
+                <label>Post body:</label>
+                <textarea
+                  required
+                ></textarea>
+                <label>Post author:</label> 
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Post</button>
+            </form>
         </div>
     );
 }
